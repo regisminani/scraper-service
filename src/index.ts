cf13914f10e1cb28f3327e0726c0f96b15516507
ll_git <minareg37@gmail.com>
2025-09-10 19:34:24 +0200

    fixing possible bug

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import { scrapeFacebookGroup } from "./scraper";
+import cors from "cors";
+
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 
 const SCRAPER_SECRET = process.env.SCRAPER_SECRET;
 
